4e3ec88e0679ef48d72d5f8df766db9012631d19
FioriMartins <caiofiorigamer1234@outlook.com>
2025-05-17 14:09:26 -0300

    Variáveis são imutáveis por padrão e RUST já declara variáveis pensando em ENCODE

--- main.rs
+++ main.rs
@@ -2,6 +2,15 @@ fn main() {
     let nomevariavel:i32 = 128;
     println!("Variavel = {}, tamanho = {} bytes", nomevariavel, std::mem::size_of_val(&nomevariavel));
 
-    let decimal = 2.5;
+    let decimal:f32 = 2.5;
     println!("decimal = {}", decimal);
+
+    // variaveis no RUST são imutáveis
+    // mas você pode torná-la mutável com 'mut' antes da declaração do nome
+    let mut booleana = true;
+    booleana = false;
+    println!("Valor= {}, Tamanho booleana = {}", booleana, std::mem::size_of_val(&booleana));
+
+    let letra:char = 'C';
+    println!("Tamanho do char = {}", std::mem::size_of_val(&letra));
 }
